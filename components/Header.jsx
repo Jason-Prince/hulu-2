718b3501f8fc9c12e54a089fb7842c6fa569860f
Jason Prince <jasoncprince@gmail.com>
2021-11-06 17:07:08 -0600

    Add: header with icons, hover, and sm media query

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,25 @@
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
 import HeaderItem from "./HeaderItem";
 import Image from "next/image";
 
 function Header() {
   return (
-    <header className="">
-      <div>
-        <HeaderItem />
+    <header className="flex flex-col m-5 justify-between items-center h-auto sm:flex-row">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
       <Image src="/hulu-Green-digital.svg" width={200} height={100} />
     </header>
